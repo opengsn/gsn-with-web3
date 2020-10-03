9236ced532fe0ad261b6473262f0c225122990aa
Dror Tirosh <dror@opengsn.org>
2020-10-04 01:36:07 +0300

    update to gsn 2.0.1

--- README.md
+++ README.md
@@ -1,32 +1,31 @@
-# Sample GSN project with ethers.
+# Sample GSN project with web3
 
-This is a simple project to demonstrate using OpenGSN with truffle and web3
+This is a simple project to demonstrate using OpenGSN with Truffle and web3
 
 Usage:
 ```
 yarn install
 
-ganache-cli & (or in another terminal)
-
 yarn test
 ```
 
-The test brings up a full GSN envorment by calling `startGsn()`. It deploy the contracts and starts a relay inside your test.
+The test can be done in 3 modes:
+
+1. self-contained test - `yarn test`.
+
+2. start `ganache` in another window, and then `npx gsn start` in another to install GSN and bring up a relayer. 
+  Then tell the test NOT to start GSN internally, by setting yarnWithGanache=true. The test will still need to load the Forwarder and RelayHub addresses.
+  Then run the test with `truffle test`
+
+3. run `yarn testkovan` to start the test against a deployed network (kovan)
+
+
+The test thus shows how to start GSN within the test (using `startGsn()`) and how to refernce GSN started in another terminal.
+
 The test then shows how to configure your web3 contracts to use GSN.
 
 The tests themselves merely verify that the call was relayed (caller didn't pay..) and that indeed it can see the caller's address (using `_msgSender()`)
 
 It shows using both truffle's TruffleContract and web3.Contract
 
-You can also start GSN outside the test:
-- start GSN from another window, by calling: 
-  ```
-  npx gsn start localhost
-  ```
-  
-- In the test, remove the `startGSN()` in the test.
-- Update the test to read the relayHub address (and other deployed components):
-  ```
-  relayHubAddress = require('./build/gsn/RelayHub').address
-  ```
 
--- test/test-with-truffle.js
+++ test/test-with-truffle.js
@@ -7,57 +7,89 @@ Counter = artifacts.require( 'Counter')
 // IPaymaster = artifacts.require( 'IPaymaster')
 CounterArtifact = require( '../build/contracts/Counter')
 
+let counterAddress
 describe( 'using truffle\'s TruffleContract', ()=> {
   let counter
   let from
   let forwarder
   let paymasterAddress
+  let netid
   before(async function () {
-    this.timeout(20000) //startGsn takes few seconds
-    if ( process.argv.includes('kovan')) {
+    this.timeout(60000) //startGsn takes few seconds
+    netid = await web3.eth.net.getId()
+  	console.log( 'using network', netid) 
+  
+    switch (netid) {
+      case 42:
+        paymasterAddress = '0x083082b7Eada37dbD8f263050570B31448E61c94'
+        forwarder = '0x0842Ad6B8cb64364761C7c170D0002CC56b1c498'
+        counterAddress = '0x60180E5a653b475dAb7F5a92523A88d141215355'
+        break
 
-      paymasterAddress = '0x9940c8e12Ca14Fe4f82646D6d00030f4fC3C7ad1'
-      forwarder = '0x663946D7Ea17FEd07BF1420559F9FB73d85B5B03'
+      case 4:
+        paymasterAddress = '0x43d66E6Dce20264F6511A0e8EEa3f570980341a2'
+        forwarder = '0x956868751Cc565507B3B58E53a6f9f41B56bed74'
+        counterAddress = '0x74440273b6442C879A7174Fed79b18720B4f2218'
+        break
 
-    } else {
-      const yarnWithGanache=false
-      if ( yarnWithGanache ) {
+      case 3:
 
-        //gsn started with "yarn ganache-with-gsn"
-        paymasterAddress = require( '../build/gsn/Paymaster.json').address
-        forwarder = require( '../build/gsn/Forwarder.json').address
-      } else {
+        paymasterAddress = '0x8057c0fb7089BB646f824fF4A4f5a18A8d978ecC'
+        forwarder: '0x25CEd1955423BA34332Ec1B60154967750a0297D'
+        counterAddress = '0xDF387A17FD0dC5dEcfEA385e4e336F947363831b'
+        break
 
-        //only ganache is running. start GSN with each test:      
-        let env = await GsnTestEnvironment.startGsn('localhost')
-        paymasterAddress = env.deploymentResult.naivePaymasterAddress
-        forwarder = env.deploymentResult.forwarderAddress
-      }
+      default:
+
+        const yarnWithGanache=false
+        if ( yarnWithGanache ) {
+
+          //gsn started with "yarn ganache-with-gsn"
+          paymasterAddress = require( '../build/gsn/Paymaster.json').address
+          forwarder = require( '../build/gsn/Forwarder.json').address
+
+        } else {
+
+          //only ganache is running. start GSN with each test:      
+          let env = await GsnTestEnvironment.startGsn('localhost')
+          paymasterAddress = env.deploymentResult.naivePaymasterAddress
+          forwarder = env.deploymentResult.forwarderAddress
+
+        }
     }
 
-
     const config = await resolveConfigurationGSN(web3.currentProvider, {
-        paymasterAddress, 
-        forwarderAddress: forwarder, 
-	verbose: true
+      paymasterAddress, 
+      // forwarderAddress: forwarder, 
+    	verbose: true
     })
+    console.log( 'config=',config)
     provider = new Gsn.RelayProvider(web3.currentProvider, config )
 
+    provider.registerEventListener(console.log)
     //create a new gasless account:
     from = web3.utils.toChecksumAddress(provider.newAccount().address)
   })
 
   it( 'run with TruffleContract', async function() {
-    this.timeout(20000)
+    this.timeout(60000)
 
-    counter = await Counter.new(forwarder)
+//    counter = await Counter.new(forwarder)
 
-    if ( !counter && process.argv.includes('kovan') )
-      counter = await Counter.at('0xDF387A17FD0dC5dEcfEA385e4e336F947363831b')
-    console.log( 'created counter=', counter.address)
+    if ( counterAddress ) {
+      //on testnet. use existing contract
+      counter = await Counter.at(counterAddress)
+      console.log( 'using counter at address=', counter.address)
+    } else {
 
-    //NOTE: this is required only it truffle tests: the "artifacts.require" is called
-    // before we could update the global web3's provider,
+        //deploy on ganache
+        counter = await Counter.new(forwarder)
+        console.log( 'deployed counter at address=', counter.address)
+    }
+    console.log( 'paymaster=', paymasterAddress)
+
+    //NOTE: this is required only in truffle tests: the "artifacts.require" is called
+    // before we could update the global web3's provider
     Counter.web3.setProvider(provider)
 
     await counter.increment({from, gas:1e6})
@@ -65,17 +97,20 @@ describe( 'using truffle\'s TruffleContract', ()=> {
   })
 
   it( 'run with web3.Contract', async function () {
-    this.timeout(20000)
-    const deployAccount = (await web3.eth.getAccounts())[5]
-    console.log( 'bal ', deployAccount, await web3.eth.getBalance(deployAccount))
-    //create a contract and deploy it - without GSN
-    const deployCounter = new web3.eth.Contract( CounterArtifact.abi )
-    const deployed = await deployCounter.deploy({arguments:[forwarder], data:CounterArtifact.bytecode} ).send({from:deployAccount, gasLimit:1e6, gasPrice:1})
+    this.timeout(60000)
+    if ( !counterAddress ) {
+      const deployAccount = (await web3.eth.getAccounts())[0]
+      console.log( 'bal ', deployAccount, await web3.eth.getBalance(deployAccount))
+      //create a contract and deploy it - without GSN
+      const deployCounter = new web3.eth.Contract( CounterArtifact.abi )
+      const deployed = await deployCounter.deploy({arguments:[forwarder], data:CounterArtifact.bytecode} ).send({from:deployAccount, gasLimit:1e6})
+      counterAddress = deployed.options.address
+    }
 
     web3.setProvider(provider)
     //now any contract created with this web3 instance is GSN-enabled
 
-    const counter = new web3.eth.Contract( CounterArtifact.abi, deployed.options.address )
+    const counter = new web3.eth.Contract( CounterArtifact.abi, counterAddress )
 
     await counter.methods.increment().send({from, gas:1e6})
     assert.equal( from, await counter.methods.lastCaller().call() )
